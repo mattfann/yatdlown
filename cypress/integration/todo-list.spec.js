0f53553617997b8bda6c00b6446cda9a0c87e580
Petra Boldi <boldi.petra@gmail.com>
2021-04-14 18:01:39 +0200

    test-driving todo-list

--- cypress/integration/todo-list.spec.js
+++ cypress/integration/todo-list.spec.js
@@ -14,4 +14,23 @@ describe('todo-list', function(){
     it('shows textbox', function(){
         cy.get('#textbox').should('be.empty')
     })
+       it('shows todo item on page', function(){
+        cy.get('#textbox').type('my first todo')
+        cy.get('#save').click()
+        cy.contains('my first todo')
+    })
+        it('the item is being completed', function(){
+        cy.get('#textbox').type('my first todo')
+        cy.get('#save').click()
+        cy.get('[type="checkbox"]').click()
+        cy.get('label').should('have.css', 'text-decoration','line-through rgb(0, 0, 0)')
+        
+    })
+      it('clears item from the page', function(){
+        cy.get('#textbox').type('my first todo')
+        cy.get('#save').click()
+        cy.get('#clear').click()
+        cy.get('my first todo').should('not.exist');
+    })
+
 })
--- src/components/TodoList.spec.js
+++ src/components/TodoList.spec.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import TodoList from './TodoList.js';
+import userEvent from '@testing-library/user-event'
+// import '@testing-library/jest-dom'
 
 it('renders Heading', () => {
   render(<TodoList />);
@@ -20,4 +22,29 @@ it('renders TextBox', () => {
   let result = render(<TodoList />) ;
   const linkElement = result.container.querySelector('#textbox');
   expect(linkElement).toBeInTheDocument();
+});
+it('displays todo item', () => {
+  render(<TodoList />) ;
+   userEvent.type(screen.getByRole('textbox'), 'my first todo');
+   userEvent.click(screen.getByText('Save'))
+   const linkElement = screen.getByText("my first todo")
+   expect(linkElement).toBeInTheDocument();
+});
+// it('todo is completed', () => {
+//   render(<TodoList />) ;
+//    userEvent.type(screen.getByRole('textbox'), 'my first todo');
+//    userEvent.click(screen.getByText('Save'))
+//    userEvent.click(screen.getByLabelText('my first todo'))
+//    const linkElement = screen.getByText('my first todo')
+//    expect(linkElement).toHaveStyle({
+//      text-decoration: 'line-through'});
+// });
+
+it('clears todo item', () => {
+  render(<TodoList />) ;
+   userEvent.type(screen.getByRole('textbox'), 'my first todo');
+   userEvent.click(screen.getByText('Save'))
+   userEvent.click(screen.getByText('Clear'))
+   const linkElement= screen.queryByText('my first todo')
+   expect(linkElement).not.toBeInTheDocument();
 });
